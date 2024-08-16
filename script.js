166ff75cac17afd61bdd6ffff8edcf994a4b5b9e
iqrahy <iqrahyderxwave@gmail.com>
2024-08-16 13:30:16 +0500

    first problem solve

---
+++ script.js
@@ -0,0 +1,7 @@
+/* 1. 
+a. Selects an element by its ID and changes its background color to lightblue. 
+*/
+
+const getHeading = document.querySelector('#javascript');
+      getHeading.style.backgroundColor = 'lightblue';
+console.log(getHeading);
