0abe372d9c1b7872d928a233ec1a3a08efaaf820
iqrahy <iqrahyderxwave@gmail.com>
2024-08-16 13:54:24 +0500

    problem 2 solved

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-/* 1. 
-a. Selects an element by its ID and changes its background color to lightblue. 
-*/
+// 1.
+// a. Selects an element by its ID and changes its background color to lightblue.
 
-const getHeading = document.querySelector('#javascript');
-      getHeading.style.backgroundColor = 'lightblue';
+const getHeading = document.querySelector("#javascript");
+getHeading.style.backgroundColor = "lightblue";
 console.log(getHeading);
+
+// b. Selects all elements with the class name "highlight" and changes their font size to 20px.
+
+const items = document.querySelectorAll(".highlight");
+items.forEach((a) => {
+  a.style.fontSize = "20px";
+  console.log(a);
+});
